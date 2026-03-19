7bec4415b2224ad6282923235ec49c994d550c0a
Aryan Pandey <engg.aryanpandey@gmail.com>
2026-03-19 05:08:50 +0000

    done work 19/03/2026

--- tool_based/src/App.jsx
+++ tool_based/src/App.jsx
@@ -9,16 +9,31 @@ import Html from "./Component/html";
 function add(a, b) {
   return a + b;
 }
+function mul(a,b,c){
+  return a*b*c ;
+}
 
 //Here , it is a Component
 function App() {
   const result = add(5, 3);
+  const name="Aryan";
+  let x=90;
+  let y=56;
+  var a=20;
+  var a=40;
   return (
     <>
-      <h1>This is a simple sentence.</h1>
+      <h1>Aryan Pandey(2300101530035)</h1>
+      <h2>Hello I am {name}</h2>
+      <h3>The Difference of two number {x-y}</h3>
+      <h3>var value : {a}</h3>
+      <h3> Multiplication : {mul(5,2,7)}</h3>
+
+
       <h2>Addition Result: {result}</h2>
       <Assignment />
       <Html />
+
     </>
   );
 }
